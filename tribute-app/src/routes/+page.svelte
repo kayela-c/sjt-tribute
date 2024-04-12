502124ff5d13b079c926d7885576d336b7bd579e
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-04-11 23:20:27 -0500

    finish padding

--- tribute-app/src/routes/+page.svelte
+++ tribute-app/src/routes/+page.svelte
@@ -3,116 +3,114 @@
 </script>
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<div id="head" class="card">
-			
-				<h1 class="main-title h1">Soujurner Truth</h1>
-				<img src="/src/potrait.jpg" alt="Sojourner Truth" class="object-contain"/>
+		<div id="head" class="card object-center py-8">
+				<h1 class="main-title h1 card-header">Soujurner Truth</h1>
+				<img src="/src/potrait.jpg" alt="Sojourner Truth" class="h-auto m-auto px-8"/>
 				<p id="img-caption">Abolitionist, Author, Human Rights Activist.</p>
-			
-		</div>
-		<div id="tribute-info">
-			<p class="timeline-points">
-			  <span class="h6">1792</span> - Isabella Baumfree (Sojorner
-			  Truth) born into slavery on the Hardenbergh estate, Swartekill,
-			  Ulster County, New York
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1806</span> - Sold at auction for $100 by
-			  John Neely, near Kingston, NY
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1808</span> - Sold for $105 by Martinus
-			  Schryver of Kingston, NY, staying there about 18 months.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1810</span> - Sold for $175 by John Dumont,
-			  New Paltz, NY
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1815</span> - Marries Thomas, a fellow slave.
-			  Has five children.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1826</span> - Isabella escapes to freedom
-			  with infant daughter, Sophia -- she had to leave the other children
-			  behind because they were not legally freed in the emancipation order
-			  until they had served as bound servants until their twenties.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">July 4, 1827</span> - New York state
-			  emancipates slaves born after 1799.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1827-1828</span> - Successfully sues a white
-			  man for illegally selling her son Peter out of state.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1829</span> - Moves to New York City with her
-			  son Peter.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1843</span> - At age 46, Isabella adopts the
-			  name Sojourner Truth, leaves New York and travels to Springfield,
-			  Mass.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1847</span> - Works as housekeeper for George
-			  Benson, brother-in-law of William Lloyd Garrison, in Northampton.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1850</span> - Sojourner dictates her
-			  autobiography, The Narrative of Sojourner Truth. It is published by
-			  William Lloyd Garrison. She speaks at the first national Women's
-			  Rights Convention, in Worcester, MA. She is the only black present..
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1851</span> - Delivers the famous "Ain't I a
-			  Woman" speech in Akron, OH.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1852</span> - in August, attends abolitionist
-			  meeting in Salem, Ohio, where she confronts Frederick Douglass,
-			  asking "Is God gone?"
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1853</span> - in October, speaks at
-			  suffragist "mob convention" at Broadway Tabernacle, New York city --
-			  visits Harriet Beecher Stowe in Andover, Mass.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1864</span> - Visits President Abraham
-			  Lincoln at the White House. Sojourner said, "I must say, and I am
-			  proud to say, that I never was treated by anyone with more kindness
-			  and politeness than were shown to me by that great and good man,
-			  Abraham Lincoln."
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1865</span> - Assigned to work at Freedman's
-			  Hospital in Washington -- rides the Washington, DC, streetcars to
-			  force their desegregation.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1867</span> - Moves from Harmonia into Battle
-			  Creek, converting Merritt "barn" on College Street into her house.
-			  Travels to Rochester, New York, and Washington, DC to help freed
-			  slaves -- visits suffrage activist Elizabeth Cady Stanton.
-			</p>
-			<p class="timeline-points">
-			  <span class="h6">1878</span> - 81-year-old Sojourner is a
-			  delegate to the Women's Rights Convention in Rochester, NY.
-			</p>
-  
-			<p class="timeline-points">
-			  <span class="h6">November 26, 1883</span> -- Sojourner Truth
-			  dies at her College Street home in Battle Creek, Michigan
-			</p>
-			<div class="center-text anchor">
-			  <a
-				href="https://en.wikipedia.org/wiki/Sojourner_Truth"
-				target="_blank"
-				>Click to read more information about Soujurner Truth</a
-			  >
-			</div>
+				<div id="tribute-info">
+					<p class="timeline-points">
+					  <span class="h6">1792</span> - Isabella Baumfree (Sojorner
+					  Truth) born into slavery on the Hardenbergh estate, Swartekill,
+					  Ulster County, New York
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1806</span> - Sold at auction for $100 by
+					  John Neely, near Kingston, NY
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1808</span> - Sold for $105 by Martinus
+					  Schryver of Kingston, NY, staying there about 18 months.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1810</span> - Sold for $175 by John Dumont,
+					  New Paltz, NY
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1815</span> - Marries Thomas, a fellow slave.
+					  Has five children.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1826</span> - Isabella escapes to freedom
+					  with infant daughter, Sophia -- she had to leave the other children
+					  behind because they were not legally freed in the emancipation order
+					  until they had served as bound servants until their twenties.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">July 4, 1827</span> - New York state
+					  emancipates slaves born after 1799.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1827-1828</span> - Successfully sues a white
+					  man for illegally selling her son Peter out of state.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1829</span> - Moves to New York City with her
+					  son Peter.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1843</span> - At age 46, Isabella adopts the
+					  name Sojourner Truth, leaves New York and travels to Springfield,
+					  Mass.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1847</span> - Works as housekeeper for George
+					  Benson, brother-in-law of William Lloyd Garrison, in Northampton.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1850</span> - Sojourner dictates her
+					  autobiography, The Narrative of Sojourner Truth. It is published by
+					  William Lloyd Garrison. She speaks at the first national Women's
+					  Rights Convention, in Worcester, MA. She is the only black present..
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1851</span> - Delivers the famous "Ain't I a
+					  Woman" speech in Akron, OH.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1852</span> - in August, attends abolitionist
+					  meeting in Salem, Ohio, where she confronts Frederick Douglass,
+					  asking "Is God gone?"
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1853</span> - in October, speaks at
+					  suffragist "mob convention" at Broadway Tabernacle, New York city --
+					  visits Harriet Beecher Stowe in Andover, Mass.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1864</span> - Visits President Abraham
+					  Lincoln at the White House. Sojourner said, "I must say, and I am
+					  proud to say, that I never was treated by anyone with more kindness
+					  and politeness than were shown to me by that great and good man,
+					  Abraham Lincoln."
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1865</span> - Assigned to work at Freedman's
+					  Hospital in Washington -- rides the Washington, DC, streetcars to
+					  force their desegregation.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1867</span> - Moves from Harmonia into Battle
+					  Creek, converting Merritt "barn" on College Street into her house.
+					  Travels to Rochester, New York, and Washington, DC to help freed
+					  slaves -- visits suffrage activist Elizabeth Cady Stanton.
+					</p>
+					<p class="timeline-points">
+					  <span class="h6">1878</span> - 81-year-old Sojourner is a
+					  delegate to the Women's Rights Convention in Rochester, NY.
+					</p>
+		  
+					<p class="timeline-points">
+					  <span class="h6">November 26, 1883</span> -- Sojourner Truth
+					  dies at her College Street home in Battle Creek, Michigan
+					</p>
+					<div class="center-text anchor">
+					  <a
+						href="https://en.wikipedia.org/wiki/Sojourner_Truth"
+						target="_blank"
+						>Click to read more information about Soujurner Truth</a
+					  >
+					</div>
+				</div>
 		</div>
 	</div>
 </div>
