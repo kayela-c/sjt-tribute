85d75e98664dcd6e806f1e102fc845c34c0122eb
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-04-11 21:40:39 -0500

    updated styling

--- tribute-app/src/routes/+layout.svelte
+++ tribute-app/src/routes/+layout.svelte
@@ -20,6 +20,9 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	//css file
+	import "../app.css";
 </script>
 
 <slot />
